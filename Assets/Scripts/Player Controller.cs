fb45b8f9b662f9967029b318a753a7526751fa9f
ShinjiEvangelion <killuasfilms@gmail.com>
2016-07-25 16:33:31 -0500

    Minigame

---
+++ Assets/Scripts/Player Controller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Rb.AddForce (Vector3); 
+	{
+}	
